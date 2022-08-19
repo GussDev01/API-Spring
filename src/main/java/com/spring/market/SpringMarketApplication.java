e43434ca7a5104084959206240e2792b723dc214
Gustavo D. Angel <parrot@localhost.localdomain>
2022-08-19 15:06:48 -0500

    primer commit

---
+++ src/main/java/com/spring/market/SpringMarketApplication.java
@@ -0,0 +1,13 @@
+package com.spring.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/market/SpringMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
