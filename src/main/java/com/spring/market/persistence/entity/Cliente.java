2d89eec5c3d0fb43022298b5bfb29d3d678e6893
Gustavo D. Angel <gdelangelf2100@alumno.ipn.mx>
2022-08-19 23:39:17 -0500

    Base de datos enlazada a Java, junto con el mapeo de las tablas como clases.

---
+++ src/main/java/com/spring/market/persistence/entity/Cliente.java
@@ -0,0 +1,73 @@
+package com.spring.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Integer celular;
+
+    private String direccion;
+
+    @Column(name = correo_electronico)
+    private String correoElectronico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
